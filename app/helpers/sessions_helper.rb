bca43cab66c0f83e8892715878d0d803f31a7c7d
tomooshima0828 <tomo.oshima0828@gmail.com>
2020-06-02 13:38:35 +0000

    finish editing users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,6 +17,7 @@ module SessionsHelper
   end
   
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
@@ -33,8 +34,21 @@ module SessionsHelper
     end
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
   def logged_in?
     !current_user.nil?
   end  
+  
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: "Sample User",
+User.create!(name: "Administrator",
              email: "sample@email.com",
              password: "password",
-             password_confirmation: "password"
-             )
+             password_confirmation: "password",
+             admin: true)
+
+60.times do |n|
+  name = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
